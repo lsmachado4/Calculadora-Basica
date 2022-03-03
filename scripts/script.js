47122a8b72cff88d81678ab6e9544c7e5fc03bc8
Lucas Machado <machado051@gmail.com>
2022-03-03 00:59:38 -0300

    Estilização da página

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+function Calculadora(num1,num2){        
+   num1= document.getElementById("num1").value //document.getElementById pega um documento que está lincado no html pelo seu ID (identificador). E o Value pega o valor que é direcionado pelo usuário.
+   num2 = document.getElementById("num2").value
+}
+
