6afddea1bc6ffdee56e5865bef133850b4b917f3
Lucas Machado <machado051@gmail.com>
2022-05-27 15:57:26 -0300

    AlterandoErroDeResultado. Acrescentando Operações.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,39 @@
-function Calculadora(num1,num2){        
-   //document.getElementById pega um documento que está lincado no html pelo seu ID (identificador). E o Value pega o valor que é direcionado pelo usuário. O <parseFloat> faz um numero que está sendo reconhecido como string ser um número;
-   nu1=  parseFloat(document.getElementById("num1").value)
+function calcular(num1, num2) {
+   //document.getElementById pega um documento que está lincado no html pelo seu ID (identificador). E o Value pega o valor que é direcionado pelo usuário. O <parseFloat> faz um numero que está sendo reconhecido como string passe a ser reconhecido como um número;
+   nu1 = parseFloat(document.getElementById("num1").value)
    nu2 = parseFloat(document.getElementById("num2").value)
    seletor = document.getElementById("seletor").value
 
-   switch(seletor){
+   switch (seletor) {
       case '+':
-         calculo = (nu1+nu2)
-         document.getElementById("resultado").innerHTML = ` O resultado da soma de ${num1} + ${num2} é: ${calculo}` //innerHTMLBusca elementos HTML 
+         calculo = (nu1 + nu2)
+         document.getElementById("resultado").innerHTML = ` O resultado da soma de ${nu1} + ${nu2} é: ${calculo}` //innerHTMLBusca elementos HTML 
          break
+
+      case '-':
+         calculo = (nu1 - nu2)
+         document.getElementById("resultado").innerHTML = ` O resultado da subtração de ${nu1} - ${nu2} é: ${calculo}` //innerHTMLBusca elementos HTML 
+         break
+      case '*':
+         calculo = (nu1 * nu2)
+         document.getElementById("resultado").innerHTML = ` O resultado da multiplicação de ${nu1} x ${nu2} é: ${calculo}` //innerHTMLBusca elementos HTML 
+         break
+      case '/':
+
+         calculo = Math.round((nu1 / nu2)) //arredonda o valor da dízima. 
+         if (Number.isNaN(calculo)) {
+            document.getElementById("resultado").innerHTML = ` Insira divisores válidos!`
+         } else
+            if (nu2 == 0) {
+               document.getElementById("resultado").innerHTML = ` Insira divisor válido!`
+
+            } else {
+               document.getElementById("resultado").innerHTML = ` O resultado da divisão de ${nu1} / ${nu2} é: ${calculo}` //innerHTMLBusca elementos HTML 
+               break
+            }
    }
 
 
 
 }
-
+//Colocar calculadora para porcentagem e mudar tema com foto ; e  adicionar atualização automática.
